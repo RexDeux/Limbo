e98ed672bc4da1e641c48565ca0acc37ac5fb1e0
Nuno Costa <nm.cdc@hotmail.com>
2021-10-31 01:20:01 +0100

    Add files via upload

---
+++ Simple_Calculator.rb
@@ -0,0 +1,17 @@
+
+    #Create a simple method with 
+def math(number1, number2, operator)
+   case operator
+   when "+"
+        number1 + number2
+    when "-"
+        number1 - number2
+    when "*"
+        number1 * number2
+    when "/"
+        number1 / number2
+    end
+end
+
+
+    
